6134665e97e35fcb0e72dce48322fc1a71affc25
Tiago Fonseca <tiagopereirafonseca@gmail.com>
2017-04-02 01:11:48 -0300

    primeiro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Renderer } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+
+  pessoa: FormGroup;
+  attr: any;
+  
+  constructor( private fb: FormBuilder, private renderer: Renderer ) { }
+
+  ngOnInit(): void {
+    this.pessoa = this.fb.group({
+      nome : new FormControl('',[ Validators.required, Validators.minLength(2) ]),
+      cpf : new FormControl('',[ Validators.required, Validators.minLength(11), Validators.maxLength(11), Validators.pattern('[0-9]+') ]),
+      email : new FormControl('',[ Validators.required, Validators.minLength(2) ])
+    });
+  }
+
+    onSubmit(pessoa): void {
+      this.attr = new Array();//não curti muito essa ideia de instanciar em todo click do button
+      for(let attr in pessoa.controls){
+        if(pessoa.controls[attr].invalid){          
+          this.attr.push(attr);
+          this.setElement(attr, 'red');
+        }else{
+          this.setElement(attr, 'green');
+        }
+      }
+    }
+
+    setElement(attr: string, color: string): void {
+          this.renderer.setElementStyle(this.renderer.selectRootElement('#'+attr), 'border-color', color);
+    }
+
+}
